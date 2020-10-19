e67d09c50295c309219ffd8a99a266ba2fee144e
Ingmar Kole <ikole@student.codam.nl>
2020-10-19 18:14:36 +0200

    redirections works in one direction

--- srcs/execute/redirect.c
+++ srcs/execute/redirect.c
@@ -29,26 +29,33 @@ t_cmd	*redir_input(t_cmd *cmd)
 
 t_cmd	*redir_output(t_cmd *cmd)
 {
-	int	fd;
-	int backup;
+	int		fd;
+	int 	backup;
+	t_cmd	*head;
 
 	backup = dup(STDOUT_FILENO);
-	if (backup < 0)
-		exit (errors("dup failed", 1));
-	if (cmd->type == trunc)
-		fd = open(cmd->next->arg[0], O_CREAT | O_TRUNC | O_RDWR, 0644);
-	else
-		fd = open(cmd->next->arg[0], O_CREAT | O_APPEND | O_RDWR, 0644);
-	if (fd < 0)
-		errors("Error opening file or directory", 1); //
-	if (dup2(fd, STDOUT_FILENO) == -1)
-		exit (errors("dup2 failed", 1));
-	close(fd);
-	cmd = select_commands(cmd, false);
+	head = cmd;
+	while (cmd->type >= append)
+	{
+		if (backup < 0)
+			exit (errors("dup failed", 1));
+		if (cmd->type == trunc)
+			fd = open(cmd->next->arg[0], O_CREAT | O_TRUNC | O_RDWR, 0644);
+		else
+			fd = open(cmd->next->arg[0], O_CREAT | O_APPEND | O_RDWR, 0644);
+		if (fd < 0)
+			errors("Error opening file or directory", 1); //
+		if (dup2(fd, STDOUT_FILENO) == -1)
+			exit (errors("dup2 failed", 1));
+		close(fd);
+		cmd = cmd->next;
+	}
+	head = select_commands(head, false);
 	close(STDOUT_FILENO);
 	if (dup2(backup, STDOUT_FILENO) == -1)
 		exit (errors("dup2 failed", 1));
 	close(backup);
-	cmd = free_cmd(cmd);
-	return (cmd);
+	while (head->type >= append)
+		head = free_cmd(head);
+	return (head);
 }
